f709da363cf899a59917338acc3f6b2273ad2785
Sailaja-13 <sailaja@ramdisk.in>
2023-04-04 23:00:36 +0530

    ui changes

---
+++ src/api-service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+// import { Utility } from '../utility';
+import { URLS } from './app/urls';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  httpOptions: any
+  httpOptions1: any
+  blobHttpOptions: any
+  constructor(private http: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    this.httpOptions1 = {
+      headers: new HttpHeaders({
+        'Content-Type': 'text/plain'
+      })
+    }
+
+    this.blobHttpOptions = { headers: this.httpOptions, responseType: 'blob' }
+  }
+
+  //token
+  tokenList(data: any): Observable<any> {
+    return this.http.get<any>(URLS.TOKEN_LIST + "/" + data, this.httpOptions)
+  }
+  tokenSave(data: any): Observable<any> {
+    return this.http.post<any>(URLS.TOKEN_SAVE, JSON.stringify(data), this.httpOptions)
+  }
+  tokenListById(data: any): Observable<any> {
+    return this.http.get<any>(URLS.TOKEN_DETAILS + "/" + data, this.httpOptions)
+  }
+
+}
+
+
