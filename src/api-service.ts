c984a1926e298b3d9f071a53cc4fafb8145fada6
sailaja-13 <76735443+sailaja-13@users.noreply.github.com>
2023-04-24 20:44:42 +0530

    ui changes

--- src/api-service.ts
+++ src/api-service.ts
@@ -17,86 +17,40 @@ export class ApiService {
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'token': '28'
+        'token':URLS.TOKEN_ID,
+        'apiKey':URLS.API_KEY
       })
     }
   }
 
-  //token
-  tokenList(data: any): Observable<any> {
-    return this.http.get<any>(URLS.TOKEN_LIST + "/" + data, this.httpOptions)
-  }
-  tokenSave(data: any): Observable<any> {
-    return this.http.post<any>(URLS.TOKEN_SAVE, JSON.stringify(data), this.httpOptions)
-  }
-  tokenListById(): Observable<any> {
-    return this.http.get<any>(URLS.TOKEN_DETAILS, this.httpOptions)
+  tokenInfo(username:any): Observable<any> {
+    return this.http.get<any>(URLS.TOKEN_DETAILS+"/"+username, this.httpOptions)
   }
 
   //erc20
-  mintSave(data: any): Observable<any> {
+  mint(data: any): Observable<any> {
     return this.http.post<any>(URLS.MINT, JSON.stringify(data), this.httpOptions)
   }
   transferFromSave(data: any): Observable<any> {
     return this.http.post<any>(URLS.TRANSFER_FROM, JSON.stringify(data), this.httpOptions)
   }
-  initializeSave(data: any): Observable<any> {
-    return this.http.post<any>(URLS.INITIALIZE, JSON.stringify(data), this.httpOptions)
+  balanceOff(data: any): Observable<any> {
+    return this.http.get<any>(URLS.BALANCE_OFF+"/"+data, this.httpOptions)
   }
-  mintWithTokenURISave(data: any): Observable<any> {
-    return this.http.post<any>(URLS.MINT_WITH_TOKEN_URI, JSON.stringify(data), this.httpOptions)
-  }
-  // clientAccountBalanceSave(data: any): Observable<any> {
-  //   return this.http.post<any>(URLS.CLIENT_ACCOUNT_BALANCE, JSON.stringify(data), this.httpOptions)
-  // }
-  BalanceOffSave(data: any): Observable<any> {
-    return this.http.post<any>(URLS.BALANCE_OFF, JSON.stringify(data), this.httpOptions)
+  transfer(data: any): Observable<any> {
+    return this.http.post<any>(URLS.TRANSFER, JSON.stringify(data), this.httpOptions)
   }
   burnSave(data: any): Observable<any> {
     return this.http.post<any>(URLS.BURN, JSON.stringify(data), this.httpOptions)
   }
-  nftExistsList(): Observable<any> {
-    return this.http.get<any>(URLS.NFT_EXISTS, this.httpOptions)
+  transactions(): Observable<any> {
+    return this.http.get<any>(URLS.TRANSACTIONS_LIST, this.httpOptions)
   }
-  readNftList(): Observable<any> {
-    return this.http.get<any>(URLS.READ_NFT, this.httpOptions)
-  }
-  clientAccountIDList(): Observable<any> {
-    return this.http.get<any>(URLS.CLIENT_ACCOUNT_ID, this.httpOptions)
-  }
-  clientAccountBalance(): Observable<any> {
-    return this.http.get<any>(URLS.CLIENT_ACCOUNT_BALANCE, this.httpOptions)
-  }
-  totalSupplyList(): Observable<any> {
-    return this.http.get<any>(URLS.TOKEN_SUPPLY, this.httpOptions)
-  }
-  tokenURIList(): Observable<any> {
-    return this.http.get<any>(URLS.TOKEN_URI, this.httpOptions)
-  }
-  tokenSymbolList(): Observable<any> {
-    return this.http.get<any>(URLS.TOKEN_SYMBOL, this.httpOptions)
-  }
-  tokeNameList(): Observable<any> {
-    return this.http.get<any>(URLS.TOKEN_NAME, this.httpOptions)
-  }
-  ownerOffList(): Observable<any> {
-    return this.http.get<any>(URLS.OWNER_OFF, this.httpOptions)
-  }
-  balanceOffList(): Observable<any> {
-    return this.http.get<any>(URLS.BALANCE_OFF, this.httpOptions)
-  }
-
-  // transactionsList(): Observable<any> {
-  //   return this.http.get<any>(URLS.TRANSACTIONS_LIST, this.httpOptions)
-  // }
-  erc20UsersList(): Observable<any> {
-    return this.http.get<any>(URLS.ERC20_USERS, this.httpOptions)
+  users(): Observable<any> {
+    return this.http.get<any>(URLS.ERC721_USERS, this.httpOptions)
   }
   appUserSave(data: any): Observable<any> {
     return this.http.post<any>(URLS.APP_USER_SAVE, JSON.stringify(data), this.httpOptions)
-  }
-  appUserList(): Observable<any> {
-    return this.http.get<any>(URLS.APP_USER_LIST, this.httpOptions)
   }
 
 }
